73bbae6a54f3f4d594cddbfa9064b011852740da
KunalBishwal <159259649+KunalBishwal@users.noreply.github.com>
2024-09-28 00:58:23 +0530

    Update CheckPackage.java

--- CheckPackage.java
+++ CheckPackage.java
@@ -1,51 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package travel.management.system;
 
-import java.sql.*;
 import java.awt.*;
-import java.awt.event.*;
-
 import javax.swing.*;
 
-public class CheckPackage extends JFrame{
+public class CheckPackage extends JFrame {
     public static void main(String[] args) {
-      new CheckPackage().setVisible(true);
+        new CheckPackage().setVisible(true);
     }
 
-    CheckPackage() {    
-      setBounds(550, 220, 900, 600);  
-      setVisible(true);
-      String[] package1 = new String[]{"package1.jpg","GOLD PACKAGE","6 days and 7 Nights", "Airport Assistance at Aiport", "Half Day City Tour", "Welcome drinks on Arrival", "Daily Buffet", "Full Day 3 Island Cruise", "English Speaking Guide", "BOOK NOW", "Summer Special", "Rs 12000 only"};
-      String[] package2 = new String[]{"package2.jpg","SILVER PACKAGE","4 days and 3 Nights", "Toll Free and Entrance Free Tickets", "Meet and Greet at Aiport", "Welcome drinks on Arrival", "Night Safari", "Full Day 3 Island Cruise", "Cruise with Dinner", "BOOK NOW", "Winter Special", "Rs 25000 only"};
-      String[] package3 = new String[]{"package3.jpg","BRONZE PACKAGE","6 days and 5 Nights", "Return Airfare", "Free Clubbing, Horse Riding & other Games", "Welcome drinks on Arrival", "Daily Buffet", "Stay in 5 Star Hotel", "BBQ Dinner", "BOOK NOW", "Winter Special", "Rs 32000 only"};
-      
-      JTabbedPane tabbedPane = new JTabbedPane();
-      JPanel p1 = createPackage(package1);
-      tabbedPane.addTab("Package 1", null, p1);
-      
-      JPanel p2 = createPackage(package2);
-      tabbedPane.addTab("Package 2", null, p2);
-      
-      JPanel p3 = createPackage(package3);
-      tabbedPane.addTab("Package 3", null, p3);
-      
-      add(tabbedPane, BorderLayout.CENTER);    
-   }
-    
-    public JPanel createPackage(String[] pack){
+    CheckPackage() {
+        setBounds(550, 220, 900, 600);
+        setVisible(true);
+
+        String[] package1 = new String[]{"/resources/package1.jpg", "GOLD PACKAGE", "6 days and 7 Nights", "Airport Assistance at Aiport", "Half Day City Tour", "Welcome drinks on Arrival", "Daily Buffet", "Full Day 3 Island Cruise", "English Speaking Guide", "BOOK NOW", "Summer Special", "Rs 12000 only"};
+        String[] package2 = new String[]{"/resources/package2.jpg", "SILVER PACKAGE", "4 days and 3 Nights", "Toll Free and Entrance Free Tickets", "Meet and Greet at Aiport", "Welcome drinks on Arrival", "Night Safari", "Full Day 3 Island Cruise", "Cruise with Dinner", "BOOK NOW", "Winter Special", "Rs 25000 only"};
+        String[] package3 = new String[]{"/resources/package3.jpg", "BRONZE PACKAGE", "6 days and 5 Nights", "Return Airfare", "Free Clubbing, Horse Riding & other Games", "Welcome drinks on Arrival", "Daily Buffet", "Stay in 5 Star Hotel", "BBQ Dinner", "BOOK NOW", "Winter Special", "Rs 32000 only"};
+
+        JTabbedPane tabbedPane = new JTabbedPane();
+        JPanel p1 = createPackage(package1);
+        tabbedPane.addTab("Package 1", null, p1);
+
+        JPanel p2 = createPackage(package2);
+        tabbedPane.addTab("Package 2", null, p2);
+
+        JPanel p3 = createPackage(package3);
+        tabbedPane.addTab("Package 3", null, p3);
+
+        add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public JPanel createPackage(String[] pack) {
         JPanel p1 = new JPanel();
         p1.setLayout(null);
         p1.setBackground(Color.WHITE);
-      
-        ImageIcon i1  = new ImageIcon(ClassLoader.getSystemResource("travel/management/system/icons/"+pack[0]));
-        Image i3 = i1.getImage().getScaledInstance(550, 300,Image.SCALE_DEFAULT);
+
+        // Using getClass().getResource to load the images
+        ImageIcon i1 = new ImageIcon(getClass().getResource(pack[0]));
+        Image i3 = i1.getImage().getScaledInstance(550, 300, Image.SCALE_DEFAULT);
         ImageIcon i2 = new ImageIcon(i3);
         JLabel l1 = new JLabel(i2);
-        l1.setBounds(400,0,450,420);
+        l1.setBounds(400, 0, 450, 420);
         p1.add(l1);
 
         JLabel lblName = new JLabel(pack[1]);
@@ -98,13 +92,13 @@ public class CheckPackage extends JFrame{
         lblDeposite.setFont(new Font("Yu Mincho", Font.PLAIN, 30));
         lblDeposite.setBounds(35, 460, 400, 40);
         p1.add(lblDeposite);
-        
+
         JLabel la1 = new JLabel(pack[11]);
         la1.setForeground(Color.RED);
         la1.setFont(new Font("Yu Mincho", Font.PLAIN, 30));
         la1.setBounds(600, 460, 400, 40);
         p1.add(la1);
-        
+
         return p1;
     }
 }
